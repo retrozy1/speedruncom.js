e002e79d405e7f4f05a51daf52214c7eaafa3a6e
retrozy <retrozybetrozy@gmail.com>
2025-07-09 09:55:15 -0400

    Simplify the client; more accurate to the purpose of this package

--- src/BaseClient.ts
+++ src/BaseClient.ts
@@ -17,11 +17,6 @@ const objectToBase64 = (obj: object) => {
     return Buffer.from(jsonString).toString('base64');
 }
 
-export interface config {
-    PHPSESSID?: string;
-    userAgent?: string;
-}
-
 export class APIError extends Error {
     status: number;
 
@@ -53,55 +48,17 @@ export default class Client {
         headers: HEADERS
     });
 
-    private username!: string;
-    private password!: string;
-
-    private headers = this.axiosClient.defaults.headers.common;
-
-    constructor(config?: config) {
-        if (config) this.config(config);
-
-        this.axiosClient.interceptors.response.use(
-            (response: AxiosResponse) => response,
-            (error: AxiosError) => {
-                const data = error.response.data as { error?: string };
-                throw new APIError(data.error || 'Unknown error', error.response.status);
-            }
-        );
-    }
-
-    config(config: config) {
-        if (!isBrowser) this.headers['User-Agent'] = BASE_USER_AGENT + (config.userAgent ? `/${config.userAgent}` : '');
-
-        if (config.PHPSESSID) {
-            if (isBrowser) {
-                console.error('You cannot use a PHPSESSID to authenticate in a browser environment.');
-            } else {
-                this.headers['Cookie'] = `PHPSESSID=${config.PHPSESSID}`;
-            }
-        }
-    }
-
     async request<T>(endpoint: string, params: object = {}): Promise<T> {
         const response = await this.axiosClient.post<T>(endpoint, params);
         
         const cookie = response.headers['set-cookie'];
-        if (cookie && !isBrowser) this.headers['Cookie'] = cookie[0].split(';')[0];
+        if (cookie && !isBrowser) this.axiosClient.defaults.headers['Cookie'] = cookie[0].split(';')[0];
 
         return response.data;
     }
 
     static async request<T>(endpoint: string, params: object = {}): Promise<T> {
         return (await this.axiosClient.get(`${endpoint}?_r=${objectToBase64(params)}`)).data;
-    }
-
-    /**
-     * Attempts to remove the PHPSESSID cookie if using a browser, otherwise removes your Client's authentication.
-     */
-    async logout() {
-        if (isBrowser) return await this.request('PutAuthLogout');
-
-        delete this.headers['Cookie'];
     }
 
     // Endpoints (auto-generated with build-client)
